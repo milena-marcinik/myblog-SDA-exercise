dc8ade4a0b5a2ec926dd068541598e124e6fb82f
Milena <milena.marcinik@gmail.com>
2021-01-30 15:07:49 +0100

    kolejne

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,17 @@
 from django.db import models
 
 # Create your models here.
-class Category(models.Model):
 
+
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f"{self.name}"
 
 
 class Entry(models.Model):
-    category= models.ForeignKey(Category)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
     title = models.CharField(max_length=100)
     body = models.TextField()
     publication_datetime = models.DateTimeField(auto_now_add=True)
