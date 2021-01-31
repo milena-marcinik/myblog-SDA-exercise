c0e89b1f07c13af7ae35c34b27c8c7cc029f3888
Milena <milena.marcinik@gmail.com>
2021-01-31 10:52:53 +0100

    admin - roznemodyfikacje

--- blog/admin.py
+++ blog/admin.py
@@ -3,6 +3,22 @@ from django.contrib import admin
 # Register your models here.
 from blog.models import Entry, Category, Tag
 
-admin.site.register(Entry)
+
+@admin.register(Entry)  # wtedy nie rejestruje juz tego jak tam na dole
+class EntryAdmin(admin.ModelAdmin):  # klasa bedzie decydowala o tym jak w adminie bedzie ywgladac moja klasa
+    # konfiguracja wyswietania listy
+    list_display = ('title', 'publication_datetime', 'category')  # jakie pola maja byc wyswietlane
+    ordering = ('-id',)
+    search_fields = ('title', 'category__name')
+    list_filter = ('category', 'publication_datetime')
+    # konfiguracja edycji/tworzenia
+    # fields = (('title', 'category'),)  # jakie pola w edycji chce pokazac
+    fieldsets = (("Naglowek 1", {'description': "To jest jakis opis do naglowka 1", "fields": ('title', 'category')}),
+                 ("Naglowek 2", {'description': "To jest jakis opis do naglowka 2", "fields": ('tags',)}),
+
+    )
+
+
+# admin.site.register(Entry, EntryAdmin)
 admin.site.register(Category)
 admin.site.register(Tag)
--- blog/models.py
+++ blog/models.py
@@ -6,12 +6,21 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=30)
 
+    class Meta:
+        ordering = ("name",)
+        verbose_name = "Kategoria"
+        verbose_name_plural = "Kategorie"
+
     def __str__(self):
         return f"{self.name}"
 
 
 class Tag(models.Model):
     name = models.CharField(max_length=30)
+
+    class Meta:
+        verbose_name = "Etykieta"
+        verbose_name_plural = "Etykiety"
 
     def __str__(self):
         return f"{self.name}"
@@ -20,9 +29,14 @@ class Tag(models.Model):
 class Entry(models.Model):
     category = models.ForeignKey(Category, on_delete=models.PROTECT)
     tags = models.ManyToManyField(Tag)
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)  # editable=False we frontendzie sie tego nie bedzie pokazywac (ale trzeba skasowac w adminie z fields i list_display
     body = models.TextField()
     publication_datetime = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ("-category",)
+        verbose_name = "Wpis"
+        verbose_name_plural = "Wpisy"
 
     def __str__(self):
         return f"{self.title}"
